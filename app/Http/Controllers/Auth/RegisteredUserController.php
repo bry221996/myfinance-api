bc6929805b1848e92717fbff3429b4532f62858f
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2024-01-21 22:08:40 +0800

    Auto Create Profiles

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
@@ -30,6 +31,11 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+        ]);
+
+        $user->profiles()->create([
+            'name'=> 'Default',
+            'description' => "Default Profile"
         ]);
 
         event(new Registered($user));
